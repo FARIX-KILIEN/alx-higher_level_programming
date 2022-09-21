29f1ad6650f89a63d7298f801705425e75fec256
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-09-21 13:46:58 -0700

    some modifications in code

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import https://alx-intranet.hbtn.io/projects/231#:~:text=9%2Deaster_egg.py-,The%20Zen%20of%20Python%2C%20by%20Tim%20Peters%0A%0ABeautiful%20is%20better%20than,Namespaces%20are%20one%20honking%20great%20idea%20%2D%2D%20let%27s%20do%20more%20of%20those!,-guillaume%40ubuntu%3A~/py
+import this
